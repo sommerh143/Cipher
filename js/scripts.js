581ff2a88ab60a23be840b4119c16e062fac03f9
Rachel Schieferstein and sommer howe <sommer@>
2020-01-13 16:19:44 -0800

    added button

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,56 @@
 
 
-    function saySomething(whatToSay) {
-    alert(whatToSay);
-    };
-
-    var origsentence = prompt("Enter a sentence: ");
-    saySomething(origsentence);
-
-    console.log(origsentence);
-
-
-    var firstchar = origsentence.charAt(0);
-
-    var lastchar = origsentence.charAt(origsentence.length-1);
+   
+jQuery(document).ready(function() {
+    $("h1").click(function() {
+        alert("heaaaaderrrrrr");
+    });
     
-   var upperfirst = firstchar.toUpperCase();
-   var upperlast = lastchar.toUpperCase();
-   var middle = origsentence.substring(1, origsentence.length-1);
+    $("p").click(function() {
+        function saySomething(whatToSay) {
+            alert(whatToSay);
+            };
+        
+            var origsentence = prompt("Enter a sentence: ");
+            saySomething(origsentence);
+        
+            console.log(origsentence);
+        
+        
+            var firstchar = origsentence.charAt(0);
+        
+            var lastchar = origsentence.charAt(origsentence.length-1);
+            
+           var upperfirst = firstchar.toUpperCase();
+           var upperlast = lastchar.toUpperCase();
+           var middle = origsentence.substring(1, origsentence.length-1);
+        
+            saySomething(upperfirst);
+            saySomething(middle);
+        
+            var flipped = upperlast + upperfirst;
+            saySomething(flipped)
+        
+         
+        
+            var sentenceflip = origsentence + flipped;
+            saySomething(sentenceflip);
+        
+            var length = origsentence.length;
+            saySomething(length);
+        
+            lengthcharloc = parseInt(length) / 2;
+            saySomething (lengthcharloc);
+        
+        
+            var result = origsentence.charAt(lengthcharloc) + sentenceflip;
+            saySomething(result);
+        
+            var bonus = result.split("").reverse().join("");
+            saySomething(bonus);
+        
+        
 
-    saySomething(upperfirst);
-    saySomething(middle);
+    });
+});
 
-    var flipped = upperlast + upperfirst;
-    saySomething(flipped)
-
- 
-
-    var sentenceflip = origsentence + flipped;
-    saySomething(sentenceflip);
-
-    var length = origsentence.length;
-    saySomething(length);
-
-    lengthcharloc = parseInt(length) / 2;
-    saySomething (lengthcharloc);
-
-
-    var result = origsentence.charAt(lengthcharloc) + sentenceflip;
-    saySomething(result);
-
-    var bonus = result.split("").reverse().join("");
-    saySomething(bonus);
